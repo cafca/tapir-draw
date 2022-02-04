1ac85bf9c95ffea6f4dd746215189b4cf5a83c44
cafca <cafca@001.land>
2022-02-04 17:49:48 +0100

    Disable caching

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, DefaultOptions } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from '~/App';
 
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: 'http://localhost:2020/graphql',
+  defaultOptions,
 });
 
 ReactDOM.render(
